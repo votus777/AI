3fad6e09ee25c54e1fc1666a7f610372893513a3
chohansae@gmail.com <votus777@users.noreply.github.com>
2020-05-12 18:10:25 +0900

    Update

--- keras/keras_01.py
+++ keras/keras_01.py
@@ -14,11 +14,42 @@ model.add(Dense(4)) # 2nd 히든 레이어
 model.add(Dense(2)) #3rd "
 model.add(Dense(1)) #아웃풋 = credit (예측값)
 
+'''
+model.summary()
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #
+=================================================================
+dense_1 (Dense)              (None, 5)                 10
+_________________________________________________________________
+dense_2 (Dense)              (None, 3)                 18
+_________________________________________________________________
+dense_3 (Dense)              (None, 2)                 8
+_________________________________________________________________
+dense_4 (Dense)              (None, 1)                 3
+=================================================================
+Total params: 39
+Trainable params: 39
+Non-trainable params: 0
+
+#param = 노드에 연결된 선의 개수 = node*(node_input+1) 
+#total param = 총 연산수
+
+#여기서 +1이란, one weight of connection with bias(절편)
+
+'''
+
+
 #3. 훈련
 
 model.compile(loss='mse', optimizer='adam',metrics=['accuracy'])
 model.fit(x,y, epochs=100, batch_size=1) # x,y를 피트니스 센터에 보내서 한개씩 잘라서 100번 반복 훈련시킨다
 
+#if batch size > data set의 크기?  - > batchsize default 값 32
+
 #4. 평가 예측
 loss, acc = model.evaluate(x,y, batch_size=1)
 print("acc: ",acc)
+
+
+# 그런데 Github 푸시가 안된다 
+# 항상 나를 괴롭힌다
--- keras/keras_02.1.py
+++ keras/keras_02.1.py
@@ -3,24 +3,21 @@ from keras.layers import Dense
 import numpy as np
 
 x_train = np.array([1,2,3,4,5,6,7,8,9,10])
-y_train = np.array([1,2,3,4,5,6,7,8,9,10])
-x_test = np.array([101,102,103,104,105,106,107,108,109,110])
-y_test = np.array([101,102,103,104,105,106,107,108,109,110])
+y_train = np.array([2,3,4,5,6,7,8,9,10,11])
+x_test = np.array([11,22,33,44,55,66,77,88,99,100])
+y_test = np.array([12,23,34,45,56,67,78,89,100,101])
 
 model = Sequential()
 model.add(Dense(5, input_dim =1 , activation='relu'))
-model.add(Dense(3))
-model.add(Dense(2))
+model.add(Dense(32))
+model.add(Dense(64))
+model.add(Dense(32))
 model.add(Dense(1, activation='relu'))
 model.summary()
 
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 
-'''
-
-
-model.compile(loss='mse', optimizer='adam', metrics=['mse'])
-
-model.fit(x_train, y_train, epochs=100, batch_size=1, validation_data = (x_test, y_test))
+model.fit(x_train, y_train, epochs=200, batch_size=1, validation_data = (x_test, y_test))
 
 loss, acc = model.evaluate(x_test, y_test, batch_size =1)
 
@@ -29,8 +26,3 @@ print("결과물 : \n", output)
 
 print("loss : ", loss)
 print("acc : ", acc)
-
-
-'''
-
-
