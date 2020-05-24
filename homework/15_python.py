7daec2bf9844dd6b4b5e7b7cbf1999a2904b65fa
chohansae@gmail.com <votus777@users.noreply.github.com>
2020-05-25 02:15:05 +0900

    homework

---
+++ homework/15_python.py
@@ -0,0 +1,19 @@
+
+# 18p
+
+for i in [1,2,3,4,5]:
+    print(i)
+    for j in [1,2,3,4,5]:
+        print(j)
+        print(i + j)
+    print(i)
+print("done looping")
+
+#p21
+
+def my_print(message = "my default message"):
+    print(message)
+
+my_print("hello")
+my_print()
+
