ee2a30cfa36d3bba3fe767a7c496947a9d0562a0
chohansae@gmail.com <votus777@users.noreply.github.com>
2020-06-06 23:28:57 +0900

    update

--- homework/python_deep/ch5.py
+++ homework/python_deep/ch5.py
@@ -7,3 +7,12 @@ c = [ "dog", "blue", "yellow"]
 c = c + ["green"]  # 이떄는 append와 다르게 리스트의 결합이므올 [ ]로 감싸야한다
 # c.append("green") 이라고 해도 된다 
 
+# 131p
+
+x = 5
+while x !=0 :   # != 0  //   '0이 아닐 떄'
+    x-= 1
+    print(x)
+
+
+    
