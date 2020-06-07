4b8ec79c6535e7a696dfd04106e3f952026006b2
chohansae@gmail.com <votus777@users.noreply.github.com>
2020-06-07 23:51:53 +0900

    update

--- homework/python_deep/ch8.py
+++ homework/python_deep/ch8.py
@@ -9,6 +9,43 @@
 
 #233p  데이터와 인덱스 추출
 
-# Seris 자료형 -> series.value()로 데이터값 추출, series.index로 인덱스(라벨) 추출
+# Seris 자료형 -> series.value()로 데이터값 추출, series.index로 인덱스 추출
 
+
+#236p
+
+# series = series.drop("strawberry") 
+#  strawberry 삭제
+
+
+#237p 필터링 
+
+# series = series[series >= 5][series < 10] 
+# 조건이 여러개일 때 [] [] 을 연속해서 덧붙이면 된다. 
+
+#238p 정렬
+
+# series 인덱스 정렬 -> series.sort_index()
+# series 데이터 정렬 -> series.sort_values()
+
+# default = ascending = True ( 오름차순 )
+
+
+# Dataframe형 변수 df의 인덱스는 df.index에 행 수와 같은 길이의 리스트를 대입하며 설정할 수 있다. 
+
+#                 df의 컬럼은 df.columns에 열 수와 같은 길이의 리스트를 대입하여 설정할 수 있다.
+
+
+# df.append( series3, ignore_index = True) => 행 추가
+# df["new"] = new_column                   => 열 추가
+
+# 데이터 참조
+# loc -> 이름으로 참조
+# iloc -> 번호로 참조
+
+# df.drop()  -> 행 또는 열 삭제
+
+# df.sort_values( by = "column or column list", ascending = True) -> 지정한 열의 값을 오름차순 정렬
+
+# df = df.loc[df["apple"] >= 5]   -> df의 "apple" 열이 5 이상 값을 가진 행을 포함한 Dateframe을 df에 대입
 
