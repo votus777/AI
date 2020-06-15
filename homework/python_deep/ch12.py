048ad9503f1a84c2141083077e17a95f3fad98d4
chohansae@gmail.com <votus777@users.noreply.github.com>
2020-06-15 17:15:55 +0900

    update

--- homework/python_deep/ch12.py
+++ homework/python_deep/ch12.py
@@ -25,8 +25,32 @@ plt.bar(x,y2, bottom = y1)  # 누적막대그래프
 
 # 히스토그램 
 
-plt.hist
+data = np.random.randn(1000)
 
+plt.hist(data, bins=100, normed=True, cumulative =True) # 구간수 100, 정규화하기 , 누적화하기 
+
+# 산포도 만들기
+y = np.arange(11,21)
+z = np.arrange(11,20)
+
+
+plt.scatter(x,y, marker = "s", color = "k")  # marker -> "o" "s" " p" "*" "+" "D"
+
+
+plt.scatter(x,y, s =z)   #Z 값에 따라서 마커 크기 변화
+plt.scatter(x,y, c =z, cmap="Blues")   #Z 값에 따라서 마커 농도 변화 
+
+
+plt.colorbar() # 컬러바 표시 
+
+#원 그래프 만들기
+# plt.pie(data, labels=labels, explode= explode)  # explode =>  해당 요소 부각 explode = [ 0,0,0.10,0]
+
+plt.axis("equal") # 원형
+
+
+
+#3D 그래프 만들기
 
 
 
