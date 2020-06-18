c0195c1aab7560f586e745309bf9f85ca7efd88f
chohansae@gmail.com <votus777@users.noreply.github.com>
2020-06-18 19:54:16 +0900

    update

--- dacon/comp1/wavelet_transform.py
+++ dacon/comp1/wavelet_transform.py
@@ -21,6 +21,7 @@ from sklearn.multioutput import MultiOutputRegressor
 from pandas.plotting import scatter_matrix
 
 from sklearn.model_selection import KFold, cross_val_score
+from sklearn.metrics import mean_absolute_error
 
 
 # 데이터 
@@ -28,9 +29,6 @@ from sklearn.model_selection import KFold, cross_val_score
 train = pd.read_csv('./data/dacon/comp1/train.csv', header = 0, index_col = 0)
 test = pd.read_csv('./data/dacon/comp1/test.csv', header = 0, index_col = 0)
 submission = pd.read_csv('./data/dacon/comp1/sample_submission.csv', header = 0, index_col = 0)
-
-
-
 
 
 # 노이즈 제거   wavelet transform
@@ -44,16 +42,16 @@ test = test.fillna(method = 'bfill')
 
 
 
-bx = np.array(train.iloc[ : , 1 :71])   # src
+bx = np.array(train.iloc[ : , 1 :36])   # src
 (ca, cd) = pywt.dwt(bx, "haar")
-cat = pywt.threshold(ca, np.std(ca), mode="soft")
-cdt = pywt.threshold(cd, np.std(cd), mode="soft")
+cat = pywt.threshold(ca, np.std(ca), mode="hard")
+cdt = pywt.threshold(cd, np.std(cd), mode="hard")
 x = pywt.idwt(cat, cdt, "haar")
 
-bt = np.array(test.iloc[ : , 1 :71])   # test
+bt = np.array(test.iloc[ : , 1 :36])   # test
 (ca, cd) = pywt.dwt(bt, "haar")
-cat = pywt.threshold(ca, np.std(ca), mode="soft")
-cdt = pywt.threshold(cd, np.std(cd), mode="soft")
+cat = pywt.threshold(ca, np.std(ca), mode="hard")
+cdt = pywt.threshold(cd, np.std(cd), mode="hard")
 tx = pywt.idwt(cat, cdt, "haar")
 
 
@@ -63,7 +61,7 @@ tx = pywt.idwt(cat, cdt, "haar")
 y = np.array(train.iloc[ : , 71: ])
 
 
-print(y[-10 : -1])
+# print(y[-10 : -1])
 
 # print(x.shape)  # (10000, 36)
 # print(tx.shape)  # (10000, 36)
@@ -78,36 +76,35 @@ x_train, x_test, y_train, y_test = train_test_split(
 )
 
 # 표준화
-from sklearn.preprocessing import StandardScaler, MinMaxScaler 
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler 
 
-standard_scaler = StandardScaler()
-x_train = standard_scaler.fit_transform(x_train)
-x_test = standard_scaler.transform(x_test)
-tx = standard_scaler.fit_transform(tx)
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+tx = scaler.transform(tx)
 
 # 차원 축소
 
 from sklearn.decomposition import PCA
 from keras.metrics import mae
-pca = PCA(n_components=36)
+pca = PCA(n_components=25)
 x_train = pca.fit_transform(x_train)
 x_test = pca.transform(x_test)
-tx = pca.fit_transform(tx)
+tx = pca.transform(tx)
 
 
 
 # 모델
 
 
-model = MultiOutputRegressor(XGBRegressor(learning_rate = 0.1, max_depth = 10,n_estimators = 1000))
+model = MultiOutputRegressor(XGBRegressor(learning_rate = 0.05, max_depth = 1, n_estimators = 500,  objective='reg:tweedie'))
 
 
 # 훈련
 early_stopping = EarlyStopping(monitor='loss', patience= 20, mode ='auto')
 kfold = KFold(n_splits=10, shuffle=True) 
 
-# model.compile (optimizer='adam', loss = 'mae', metrics=['mae'])
-hist = model.fit(x_train,y_train)
+model.fit(x_train,y_train)
 
 #  verbose=1, batch_size=10,  epochs= 1000, callbacks=[early_stopping], use_multiprocessing=True, validation_split=0.25
 
@@ -119,12 +116,24 @@ score = model.score(x_test,y_test)
 # print('mae : ', mae )
 
 
-
-
 y_predict = model.predict(tx)
-
-print("y_predict : ", y_predict)
+print("score : ", score)
+print("mae : ", mean_absolute_error(y_predict[:2000],y_test))
 print(y_test)
+
+
 predict = pd.DataFrame(y_predict, columns=['hhb','hbo2','ca','na'])
 predict.index = np.arange(10000,20000)
 predict.to_csv('./data/dacon/comp1/predict.csv', index_label='id')
+
+
+# plt.scatter(y_predict[ :2000],y_test, c = "k", cmap="Blues")
+# plt.xlabel('True Values')
+# plt.ylabel('Predictions')
+# plt.axis('equal')
+# plt.axis('square')
+# plt.xlim([0,plt.xlim()[1]])
+# plt.ylim([0,plt.ylim()[1]])
+# _ = plt.plot([-10, 10], [-10, 10])
+
+# plt.show()
